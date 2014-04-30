ad69cabdc73fa1a974e4a12f395778b865ae4b09
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2014-04-30 13:47:43 +0200

    hector_sbpl_terrain_planner: Add include to cpp file

--- hector_sbpl_terrain_planner/src/discrete_space_information/environment_navxytheta_stability_lat.cpp
+++ hector_sbpl_terrain_planner/src/discrete_space_information/environment_navxytheta_stability_lat.cpp
@@ -1,5 +1,6 @@
 //Implementation for new environment, analogous to https://github.com/sbpl/sbpl/tree/master/src/discrete_space_information
 
+#include <hector_sbpl_terrain_planner/discrete_space_information/environment_navxytheta_stability_lat.h>
 
 
 
